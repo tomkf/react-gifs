95064c25d13e2aa08b23cb9f5d59b6c593dd1c41
Thomas Ferris <thomaskeithferris@gmail.com>
2019-07-25 19:39:29 -0400

    new comp.

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
 render(){
-    return ()
+    const src="https://giphy.com/gifs/templeowls-tu-temple-3o7bu2wCQjNyFnIcog"
+    return (<img src={src} className="gif" />)
 }
 }
 
